6631825c25d52bbdd1f88d30788a29a816284dc5
onahafranco <brendantochukwu@gmail.com>
2022-10-14 03:00:29 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - Printing
  * Description - 'Printing without printf'
- * Return: Always(0) 
+ * Return: Always(0)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
