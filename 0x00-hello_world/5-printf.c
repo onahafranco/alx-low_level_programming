3f8a64553660c725463de161f8fa224a911de002
onahafranco <brendantochukwu@gmail.com>
2022-10-13 21:27:25 -0700

    print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main Emtry
+ * Description - "pracicing C with prinf"
+ * Return 0 (always succeccful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	Return(0);
+}
