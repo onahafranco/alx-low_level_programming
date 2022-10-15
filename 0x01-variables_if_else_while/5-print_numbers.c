569c2fc47cb5bf8306f79bf0cdacb06ac66e6767
onahafranco <brendantochukwu@gmail.com>
2022-10-15 06:50:49 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry
+ * Return: always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		printf("%d\n", num);
+
+	return (0);
+}
