63581b3485f22474c2a0c1bf206b2a5045e31dca
onahafranco <brendantochukwu@gmail.com>
2022-10-15 17:40:28 -0700

    mirrowed alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - printing reverse alphabet
+ * Return: Always 0
+ */
+int main(void)
+{
+	char chr;
+
+	for (chr = 'z'; chr >= 'a'; chr--)
+		putchar(chr);
+	putchar('\n');
+
+	return(0);
+}
