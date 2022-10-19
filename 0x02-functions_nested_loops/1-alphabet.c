5f61d67fe3defc17e40dee5378ffff2f26e9b7ee
onahafranco <brendantochukwu@gmail.com>
2022-10-19 03:39:04 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - printing alpahbets in lower case
+ * Return: 0
+ */
+void print_alphabet(void)
+
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		_putchar(alphabet);
+
+	_putchar("\n");
+
+	return (0);
+}
